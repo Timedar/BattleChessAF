163b88f11a5c86ef9b1f82b5ce8a8bb1a2af230e
Timedar <timedar@gmail.com>
2023-08-22 20:12:50 +0200

    Fix and optimize

--- Assets/Scripts/Items/ItemsManager.cs
+++ Assets/Scripts/Items/ItemsManager.cs
@@ -1,4 +1,6 @@
-﻿namespace AFSInterview.Items
+﻿using System;
+
+namespace AFSInterview.Items
 {
 	using TMPro;
 	using UnityEngine;
@@ -11,47 +13,59 @@
 		[SerializeField] private GameObject itemPrefab;
 		[SerializeField] private BoxCollider itemSpawnArea;
 		[SerializeField] private float itemSpawnInterval;
+		[SerializeField] private LayerMask layerMask;
 
 		private float nextItemSpawnTime;
-		
+		private TextMeshProUGUI moneyText;
+		private new Camera camera;
+
+		private void Awake()
+		{
+			moneyText = FindObjectOfType<TextMeshProUGUI>();
+			camera = Camera.main;
+		}
+
 		private void Update()
 		{
 			if (Time.time >= nextItemSpawnTime)
 				SpawnNewItem();
-			
+
 			if (Input.GetMouseButtonDown(0))
 				TryPickUpItem();
-			
+
 			if (Input.GetKeyDown(KeyCode.Space))
 				inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
 
-			FindObjectOfType<TextMeshProUGUI>().text = "Money: " + inventoryController.Money;
+			moneyText.text = "Money: " + inventoryController.Money;
 		}
 
 		private void SpawnNewItem()
 		{
 			nextItemSpawnTime = Time.time + itemSpawnInterval;
-			
+
 			var spawnAreaBounds = itemSpawnArea.bounds;
 			var position = new Vector3(
 				Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
 				0f,
 				Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
 			);
-			
+
 			Instantiate(itemPrefab, position, Quaternion.identity, itemSpawnParent);
 		}
 
 		private void TryPickUpItem()
 		{
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			var layerMask = LayerMask.GetMask("Item");
-			if (!Physics.Raycast(ray, out var hit, 100f, layerMask) || !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
+			var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+			if (!Physics.Raycast(ray, out var hit, 100f, layerMask) ||
+			    !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
 				return;
-			
+
 			var item = itemHolder.GetItem(true);
-            inventoryController.AddItem(item);
-            Debug.Log("Picked up " + item.Name + " with value of " + item.Value + " and now have " + inventoryController.ItemsCount + " items");
+			inventoryController.AddItem(item);
+
+			Debug.Log(
+				$"Picked up {item.Name} with value of {item.Value} and now have {inventoryController.ItemsCount} items.");
 		}
 	}
 }
