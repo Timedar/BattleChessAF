2ce0c702ec94004ca7c17f2766d5303485b1c36c
Timedar <timedar@gmail.com>
2023-08-22 20:58:28 +0200

    Add unit parameters

---
+++ Assets/Scripts/Unit/SpecialAttack.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine.Timeline;
+
+namespace AFSInterview
+{
+	[Serializable]
+	public struct SpecialAttack
+	{
+		public float attackDamage;
+		public UnitAttributes aggainsAttribiute;
+	}
+}
